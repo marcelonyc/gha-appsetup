bb96add24217f97804d587d321c7b303e7a9bf3e
Marcelo Litovsky <12512887+marcelonyc@users.noreply.github.com>
2025-09-14 12:30:29 -0400

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -33,7 +33,6 @@ echo "Repositories: $REPOSITORIES"
 echo "$REPOSITORIES" | jq -c '.[]' | while read repo; do
   TYPE=$(echo "$repo" | jq -r '.type')
   REPO_KEY=$(echo "$repo" | jq -r '.repo_key')
-  echo "Repository Type: $TYPE, Repo Key: $REPO_KEY"
+  echo "Repository_Type=$TYPE" >> "$GITHUB_OUTPUT" 
+  echp "Repo_Key=$REPO_KEY" >> "$GITHUB_OUTPUT"
 done
-
-echo ${RESPONSE} >> "$GITHUB_OUTPUT"
