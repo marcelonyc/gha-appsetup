1a1d8677b6eec09572b7bdec1bb306ec20f6ff72
marcelonyc <marcelo.litovsky@gmail.com>
2025-11-07 16:25:11 -0500

    Added appconfig command to system

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 set -e
 
+mkdir -p ~/appconfig
+cp .env ~/appconfig/.env
+
+source ~/appconfig/.env
+
 APPLICATION_KEY=${1}
 AMS_TOKEN=${2}
 AMS_ENDPOINT=${3}
@@ -23,30 +28,28 @@ fi
 # Make the API request (replace the URL with your actual endpoint)
 RESPONSE=$(curl -s -H "Authorization: Bearer $AMS_TOKEN" "$AMS_ENDPOINT")
 
-# echo "response=$RESPONSE" 
+# RESPONSE = '[{"project_key":"dvr","project_name":"dvr","application_key":"dvr-rental","application_name":"DVR aRental","repository_key":"dvr-docker-local-all-stages","repository_name":"dvr-docker-local-all-stages","repository_type":"docker","repository_lifestage":"all"},
+# {"project_key":"dvr","project_name":"dvr","application_key":"dvr-rental","application_name":"DVR aRental","repository_key":"dvr-generic-local-all-stages","repository_name":"dvr-generic-local-all-stages","repository_type":"generic","repository_lifestage":"all"}]'
 
-# RESPONSE = [{"project_key":"dvr","project_name":"dvr","application_key":"dvr-rental","application_name":"DVR aRental","repository_key":"dvr-docker-local-all-stages","repository_name":"dvr-docker-local-all-stages","repository_type":"docker","repository_lifestage":"all"},
-# {"project_key":"dvr","project_name":"dvr","application_key":"dvr-rental","application_name":"DVR aRental","repository_key":"dvr-generic-local-all-stages","repository_name":"dvr-generic-local-all-stages","repository_type":"generic","repository_lifestage":"all"}]
-REPO_LIST=""
+cp commands/appconfig /usr/local/bin/appconfig
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to copy appconfig command to /usr/local/bin." >&2
+  exit 1
+fi
 
-echo "$RESPONSE" | jq -c --arg app_key "$APPLICATION_KEY" '.[] | select(.application_key == $app_key)' | while read -r record; do
-  repository_type=$(echo "$record" | jq -r '.repository_type')
-  repository_lifestage=$(echo "$record" | jq -r '.repository_lifestage')
-  repository_key=$(echo "$record" | jq -r '.repository_key')
-  # {
-  #   echo 'stdout<<EOF'
-    # echo ${repository_type}-${repository_lifestage}=${repository_key} | tee -a "${GITHUB_OUTPUT}"   
-  #   echo 'EOF'
-  # } >>"${GITHUB_OUTPUT}"
-  # REPO_LIST="${REPO_LIST}${repository_type}-${repository_lifestage}=${repository_key}\n"
-  echo "${repository_type}-${repository_lifestage}=${repository_key}" >> /tmp/repo_output.txt
-done
 
-echo "repo_list=/tmp/repo_output.txt" >> "${GITHUB_OUTPUT}"
+# echo "$RESPONSE" | jq -c --arg app_key "$APPLICATION_KEY" '.[] | select(.application_key == $app_key)' | while read -r record; do
+#   repository_type=$(echo "$record" | jq -r '.repository_type')
+#   repository_lifestage=$(echo "$record" | jq -r '.repository_lifestage')
+#   repository_key=$(echo "$record" | jq -r '.repository_key')
+#   echo "${repository_type}-${repository_lifestage}=${repository_key}" >> ${config_cache_file}
+# done
 
-# {
-#   echo 'stdout<<EOF'
-#   echo $REPO_LIST
-#   echo 'EOF'
-# } >>"${GITHUB_OUTPUT}"
+# Filter the RESPONSE to create a new list containing only matching application_key
+filtered_response=$(echo "$RESPONSE" | jq -c --arg app_key "$APPLICATION_KEY" '[.[] | select(.application_key == $app_key)]')
+
+# Print the filtered list for debugging or further processing
+echo $filtered_response > ${config_cache_file}
+
+# echo "repo_list=${config_cache_file}" >> "${GITHUB_OUTPUT}"
 
